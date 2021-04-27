02d7862d7d9e7a4ad83a5be2bd744f80f116735b
유현창 <uu0506uu@gmail.com>
2021-04-27 18:06:56 +0900

    Instantiate Player And AutoMove
    4/27 캐릭터 생성 및 자동으로 움직이게 스크립트 짬

---
+++ Shooting_Game_Rapa/Assets/00.MyAsset/01.Script/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //W A S D 키를 입력받아 전후좌우로 이동시킨다.
+    [SerializeField] float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += transform.up * moveSpeed * Time.deltaTime;
+        //Debug.Log(Time.deltaTime);
+    }
+}
