c30108fb93755441ff711daeb110e92688b88c4e
유현창 <uu0506uu@gmail.com>
2021-04-29 00:42:52 +0900

    Tail
    p = p0 * vt와 선형보간법을 각각을 이용한 두 방식의 꼬리 구현

---
+++ Shooting_Game_Rapa/Assets/00.MyAsset/01.Script/L_Tail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L_Tail : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, target.transform.position.x, 0.1f)
+            , Mathf.Lerp(transform.position.y, target.transform.position.y, 0.1f)
+            , Mathf.Lerp(transform.position.z, target.transform.position.z, 0.1f));
+    }
+}
---
+++ Shooting_Game_Rapa/Assets/00.MyAsset/01.Script/P_Tail.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Tail : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // p = p0 + vt
+        transform.position += (target.transform.position - transform.position) * speed * Time.deltaTime;
+
+    }
+}
